b764179bdf7c7033dbafe6f60291809d32b68620
Pellinen Juho <juho.pellinen@aalto.fi>
2021-12-29 23:32:37 +0200

    Exercises 3.1-3.8 done.

---
+++ index.js
@@ -0,0 +1,97 @@
+const { response } = require('express')
+const express = require('express')
+const morgan = require('morgan') //npm install morgan
+const app = express()
+
+morgan.token('data', function(req, res) {
+    if (req.method === 'POST'){
+        return JSON.stringify(req.body)
+    } else {
+        return ""
+    }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+app.use(express.json())
+
+let persons = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122",
+    },  
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const message = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date}</p>`
+    res.send(message)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => id === person.id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => id === person.id)
+    const personIndex = persons.indexOf(person)
+    //console.log(personIndex)
+    const newArray = persons.splice(0,personIndex, 1)
+    res.json(newArray)
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    //console.log(body)
+    const id = persons.length + 1
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id,
+    }
+    const exists = persons.find(checked => checked.name === person.name)
+
+    if (person.number === undefined) {
+        res.send({ error: "Phone number missing" })
+    } else if (exists) {
+        res.send({ error: "Such person already found"})
+    } else {
+        persons = persons.concat(person)
+        res.json(persons)
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
